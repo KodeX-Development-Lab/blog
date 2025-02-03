096052e85cc88b70a64244a716ac31afa22da9bd
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2025-02-03 14:29:26 +0630

    feat: following users posts

--- blog_api/Modules/Blog/app/Repositories/PostRepository.php
+++ blog_api/Modules/Blog/app/Repositories/PostRepository.php
@@ -1,17 +1,24 @@
 <?php
 namespace Modules\Blog\Repositories;
 
+use Modules\Blog\Models\Follow;
 use Modules\Blog\Models\Post;
 
 class PostRepository
 {
     public function getAll($request): \Illuminate\Database\Eloquent\Collection
     {
+        $following_users = $request->only_following_posts ? Follow::where('follower_id', $request->user()->id)->pluck('following_id')->toArray() : [];
+
         return Post::with(['user:id,name,username,photo', 'myReaction', 'reactionBrief'])
             ->withCount(['comments', 'reactions'])
-            ->where(function ($query) use ($request) {
+            ->where(function ($query) use ($request, $following_users) {
                 if ($request->user_id) {
                     $query->where('posts.user_id', $request->user_id);
+                }
+
+                if ($request->only_following_posts) {
+                    $query->whereIn('posts.user_id', $following_users);
                 }
 
                 if ($request->search != null) {
@@ -30,12 +37,17 @@ class PostRepository
     public function getByParams($request)
     {
         $per_page = $request->per_page ? $request->per_page : 10;
+        $following_users = $request->only_following_posts ? Follow::where('follower_id', $request->user()->id)->pluck('following_id')->toArray() : [];
 
         return Post::with(['user:id,name,username,photo', 'myReaction', 'reactionBrief'])
             ->withCount(['comments', 'reactions'])
-            ->where(function ($query) use ($request) {
+            ->where(function ($query) use ($request, $following_users) {
                 if ($request->user_id) {
                     $query->where('posts.user_id', $request->user_id);
+                }
+
+                if ($request->only_following_posts) {
+                    $query->whereIn('posts.user_id', $following_users);
                 }
 
                 if ($request->query != null) {
